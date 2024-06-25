47ad925b70f75357fbe9aeaef5f9ef2443e56dd9
Nathan Heinzmann <nathanhph@gmail.com>
2024-06-25 15:52:06 -0300

    add ads

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,8 @@
+import Script from 'next/script'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
-
-/*
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/site.webmanifest">
-<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-<meta name="msapplication-TileColor" content="#da532c">
-<meta name="theme-color" content="#ffffff">
-*/
 
 export const metadata = {
   title: 'Portfólio | Nathan Heinzmann',
@@ -27,13 +18,17 @@ export const metadata = {
     url: 'https://nathanheinzmann.com',
     type: 'website',
   },
-  
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6753416185343224"
+        crossorigin="anonymous"
+      />
     </html>
   )
 }
