4643fa0c0dc354c141b0b0952019b03a4708a4e9
Nathan Heinzmann <nathan.heinzmann@newfold.com>
2025-06-30 21:23:56 -0300

    feat: remove nextconfig options

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
 /** @type {import('next').NextConfig} */
-// const { i18n } = require('next-i18next');
 
-module.exports = {
-    // i18n: {
-    //     locales: ['en', 'br'],
-    //     defaultLocale: 'br',
-    // },
-};
+module.exports = {};
